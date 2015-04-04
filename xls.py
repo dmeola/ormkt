74f32e5d15d94e163e576d68f40a3aa8cbc7bdb6
Daniel Meola <dmeola@gmail.com>
2015-04-04 16:10:35 -0400

    using lists (arrays) to store data

--- xls.py
+++ xls.py
@@ -6,43 +6,40 @@ import pymysql
 
 
 book = xlrd.open_workbook("sales.xlsx")
-#print "The number of worksheets is", book.nsheets
-#print "Worksheet name(s):", book.sheet_names()
 sh = book.sheet_by_index(0)
-#print sh.name, sh.nrows, sh.ncols
-#print "Cell D30 is", sh.cell_value(rowx=29, colx=3)
 
-insert_values = ''
+insert_values = []
 row_values = ''
 column_names = {'Ord No','Booked Date','Order Date','Cust PO','Order Type','Hospital PO','Loc No','Site No','Cust No','Ship To Name','Item No','Description','Qty','Total','Dcode'}
 
 #skip first 11 rows because it doesn't contain relevant data
-for row_idx in range(11, 13): #sh.nrows):
+for row_idx in range(11, 13):
+    insert_values.append([])					# add a dimension for row
     for col_idx in range(0, sh.ncols):  				# Iterate through columns
         cell_obj = sh.cell(row_idx, col_idx)  			# Get cell object by row, col
-        row_values += "'{0}',".format(cell_obj.value)
+        insert_values[row_idx-11].append(cell_obj.value)
         print (cell_obj.value)
-    insert_values += '({0}),'.format(row_values)
     print "================"
-
-    #insert_values+='({0}),'.format(sh.row(rx))
 
 #MySQL connector
 conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='ormkt_db')
 
 cur = conn.cursor()
 
-statement = 'CREATE TEMPORARY TABLE IF NOT EXISTS import'
-statement = 'INSERT INTO orders (order_id, customer_id, customer_PO, order_type, order_date, booked_date) VALUES (%s, %s, %s, %s, %s, %s)'
+SQL_column_names = ', '.join('?' * len(column_names))
 
-insert_values = conn.escape('[{0}]'.format(insert_values))
+
+statement = 'CREATE TEMPORARY TABLE IF NOT EXISTS import'
+statement = 'INSERT INTO orders VALUES (%s);' %SQL_column_names
+
+#insert_values = conn.escape('[{0}]'.format(insert_values))
 
 print('INSERT VALUES')
 print(insert_values)
 
 #insertStatement = 'INSERT INTO orders (order_id, customer_id, customer_PO, order_type, order_date, booked_date)	VALUES {0}'.format(insert_values)
 
-statement = 'INSERT INTO orders (order_id, customer_id, customer_PO, order_type, order_date, booked_date) VALUES (%s, %s, %s, %s, %s, %s)'
+#statement = 'INSERT INTO orders (order_id, customer_id, customer_PO, order_type, order_date, booked_date) VALUES (%s, %s, %s, %s, %s, %s)'
 
 #insertStatement = conn.escape(insertStatement)
 
